4acaa5c927db46de5a6bd56c3734fa75d5fc5449
prkrptr <peter_parker@dlsu.edu.ph>
2025-11-07 03:15:18 +0800

    Add input handling for dungeon simulator parameters
    Introduced global variables and an inputMode() function in main.cpp to collect simulation parameters from the user, including instance limits and player roles. Updated README.md with build and run instructions for easier setup and usage.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # ps2_stdiscm
+## Build / Compilation Instructions
+Run the following commands in order:
+
+1. mkdir build
+2. cd ./build
+3. cmake ..
+4. cmake --build .
+5. cd ./Debug
+6. "ps2_stdiscm.exe" to run the program
+
+
+# Running the Program:
+1. Ensure that the config.txt file is in the directory. (There should be a copy after building using cmake)
+2. After doing step 6 from the compilation instructions, follow the instructions in the program.
+3. Choose the variation by inputting a number from 1 to 4.
+4. Wait for program to end. 
 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,76 @@
 #include <iostream>
+#include <filesystem>
+
+
+// global variables
+//n - maximum number of concurrent instances
+uint64_t n = 0;
+// t - number of tank players in the queue
+uint64_t tank = 0;
+
+// h - number of healer players in the queue
+uint64_t healer = 0;
+
+// d - number of DPS players in the queue
+uint64_t dps= 0;
+
+// TODO: change proper datatype
+// t1 - minimum time before an instance is finished
+uint64_t min_time = 0;
+
+// t2 - maximum time before an instance is finished
+uint64_t max_time = 0;
+
+void inputMode() {
+    uint64_t number= 0;
+        std::cout << "\n WELCOME TO LFG (Looking for Group) dungeon Simulator" << std::endl;
+    // NO zero input (so that a party can be made?)
+    std::cout << "Enter Max number of concurrent instance: ";
+    std::cin >> number;
+    n = number;
+    std::cin.clear();
+
+    std::cout << "Enter number of tank players: ";
+    std::cin >> number;
+    tank = number;
+    std::cin.clear();
+
+    std::cout << "Enter number of healer players: ";
+    std::cin >> number;
+    healer = number;
+    std::cin.clear();
+
+    std::cout << "Enter number of dps players: ";
+    std::cin >> number;
+    dps = number;
+    std::cin.clear();
+
+    std::cout << "Enter minimum time before an instance is finished: " ;
+    std::cin >> number;
+    min_time = number;
+    std::cin.clear();
+
+    std::cout << "Enter maximum time before an instance is finished: ";
+    std::cin >> number;
+    max_time = number;
+
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+    // validate show inputs
+    std::cout << "n = " + std::to_string(n) << std::endl;
+    std::cout << "tank = " + std::to_string(tank) << std::endl;
+    std::cout << "healer = " + std::to_string(healer) << std::endl;
+    std::cout << "dps = " + std::to_string(dps) << std::endl;
+    std::cout << "min_time = " + std::to_string(min_time) << std::endl;
+    std::cout << "max_time = " + std::to_string(max_time) << std::endl;
+
+
+
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    inputMode();
     return 0;
 }
